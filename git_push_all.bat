b72b5622e6539fc936ebf8e67db2cffef840493a
tzechiop <thasegawa90@gmail.com>
2018-01-27 01:25:28 -0700

    space echos in git_push_all

--- git_push_all.bat
+++ git_push_all.bat
@@ -2,14 +2,22 @@ echo off
 
 set arg1=%1
 
-echo ==== Git status ====
+echo ==== Old git status ====
 git status
+echo
 
 echo ==== Adding files to git ====
 git add *
+echo
+
+echo ==== New git status ====
+git status
+echo
 
 echo ==== Committing files ====
 git commit -m %arg1%
+echo
 
 echo ==== Pushing changes ====
 git push -u origin master
+echo
